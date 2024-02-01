1a8b480ded86c0aa34631a25f384c086a3433a12
Marli Mota <marli.mota42@gmail.com>
2024-02-01 00:44:06 -0300

    restclient config

--- medlab_core_package/lib/medlab_core_package.dart
+++ medlab_core_package/lib/medlab_core_package.dart
@@ -4,3 +4,5 @@ export 'src/functional_programming/either.dart';
 export 'src/functional_programming/nil.dart';
 export 'src/functional_programming/unit.dart';
 export 'src/helpers/massages.dart';
+export 'src/restClient/rest_client.dart';
+export 'src/constants/local_storage_constants.dart';
---
+++ medlab_core_package/lib/src/restClient/interceptors/auth_interceptor.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:medlab_core_package/src/constants/local_storage_constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final class AuthInterceptor extends Interceptor {
+  @override
+  Future<void> onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    final RequestOptions(:headers, :extra) = options;
+    const authHeaderKey = 'Authorization';
+    headers.remove(authHeaderKey);
+
+    if (extra case {'DIO_AUTH_KEY': true}) {
+      final sp = await SharedPreferences.getInstance();
+      headers.addAll({
+        authHeaderKey:
+            'Bearer ${sp.getString(LocalStorageConstants.acessToken)}'
+      });
+    }
+
+    handler.next(options);
+  }
+}
